7aee1ddebf62ed25ad56b0dd34dcb7bb1256bd2b
Mehul721 <jmehul721@gmail.com>
2024-08-26 22:33:09 +0530

    Initial Commit

---
+++ FloBiz Assignment/FloBiz_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloBiz_AssignmentApp.swift
+//  FloBiz Assignment
+//
+//  Created by Mehul Jain on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FloBiz_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
